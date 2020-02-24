cd376b9825139c04131105c6be28f42fe92ba7c6
GIUEcubit <g.rinaldi@ecubit.it>
2020-02-24 15:47:32 +0100

    ERRORE di SCOPE - Variabile inutilizzata

--- src/it/aleo/thinkingInJava/controllingExecution/esercizioUno/EsercizioUno.java
+++ src/it/aleo/thinkingInJava/controllingExecution/esercizioUno/EsercizioUno.java
@@ -2,6 +2,8 @@ package it.aleo.thinkingInJava.controllingExecution.esercizioUno;
 
 public class EsercizioUno {
 
+	//ERRORE di SCOPE
+	// Variabile inutilizzata
 	int i;
 
 	public static void main(String[] args) {
